65fba33aa4009ada6c669fdc6f4b9d54ec68b8b6
Taha Şahin <115729027+tahasahinn@users.noreply.github.com>
2025-07-05 15:39:22 +0300

    Update sensetive-fields.interceptor.ts

--- api/src/interceptor/sensetive-fields.interceptor.ts
+++ api/src/interceptor/sensetive-fields.interceptor.ts
@@ -3,7 +3,7 @@ import {
   ExecutionContext,
   Injectable,
   NestInterceptor,
-} from '@nestjs/common';s
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
