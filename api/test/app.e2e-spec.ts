3fe84017b468e5bbfb1cd8947a1dfd775c9ce5b5
Taha Şahin <115729027+tahasahinn@users.noreply.github.com>
2025-08-02 12:16:52 +0300

    Update app.e2e-spec.ts

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
-
+s
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
